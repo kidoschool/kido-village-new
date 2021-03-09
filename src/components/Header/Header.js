3c1837ff957c4afaba1bc564cd59c43e4ff98141
kidoschool <fauzan.falke@kido.school>
2021-03-09 18:42:04 +0530

    logo_updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header(props){
       <>
        <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-light bg-color fixed-top">
-          <NavLink className="navbar-brand pl-4" to="/"><img src={logo} alt="logo" width="150"/></NavLink>
+          <NavLink className="navbar-brand pl-4" to="/"><img src={logo} alt="logo" width="120"/></NavLink>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
